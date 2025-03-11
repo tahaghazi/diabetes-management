be9c595a43fc3ae4428851e59762c6a520ce7d8a
mohamedmostafa33 <mohamedabohamama15@gmail.com>
2025-03-11 02:48:19 +0200

    Add UserRegisterSerializer for user registration validation

---
+++ backend/api/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True, min_length=8)
+    password2 = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def validate(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError({"password" : "Passwords must match."})
+        
+        if User.objects.filter(email=data['email']).exists():
+            raise serializers.ValidationError({"email" : "This email is already in use."})
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password1']
+        )
+        return user
