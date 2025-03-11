fc30625fa4ee0683dd3e0f95b2518566d434dc85
mohamedmostafa33 <mohamedabohamama15@gmail.com>
2025-03-11 13:10:08 +0200

    Implement user login view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,8 @@
-from .serializers import UserRegisterSerializer
+from .serializers import UserRegisterSerializer, UserLoginSerializer
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
+from django.contrib.auth import login
 
 @api_view(['POST'])
 def user_register(request):
@@ -9,4 +10,15 @@ def user_register(request):
     if serializer.is_valid():
         serializer.save()
         return Response({"message" : "User registered successfully!"}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def user_login(request):
+    serializer = UserLoginSerializer(data=request.data)
+    
+    if serializer.is_valid():
+        user = serializer.validated_data['user']
+        login(request, user)  
+        return Response({"message": "Login successful!"}, status=status.HTTP_200_OK)
+    
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
