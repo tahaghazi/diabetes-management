4bcedfaef3f51abb4682f9d923fd608eff170695
mohamedmostafa33 <mohamedabohamama15@gmail.com>
2025-03-16 00:41:03 +0200

    Implemented Password Reset Feature - Added functionality for sending a password reset link via email. - Created endpoint for verifying the reset link and updating the password.

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import user_register, user_login, user_logout
+from .views import user_register, user_login, user_logout, send_reset_password_email, reset_password_confirm
 
 urlpatterns = [
     path('register/', user_register, name="user-register"),
     path('login/', user_login, name="user-login"),
     path('logout/', user_logout, name="user-logout"),
+    path('password_reset/', send_reset_password_email, name="password_reset_email"),
+    path('password_reset/<uidb64>/<token>/', reset_password_confirm, name="password_reset_confirm"),
 ]
