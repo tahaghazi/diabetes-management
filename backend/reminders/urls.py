b0d9551d3850c471ddb0996666fbcb51988edd34
mohamedmostafa33 <mohamedabohamama15@gmail.com>
2025-04-05 18:59:37 +0200

    Finish and set the daily reminders for the patient

--- backend/reminders/urls.py
+++ backend/reminders/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('create-reminder/', views.create_daily_reminder, name='create_daily_reminder'),
+    path('get-reminders/', views.get_daily_reminders, name='get_daily_reminders'),
+]
