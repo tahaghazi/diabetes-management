ab96212649e2790ede87dc14c73864a7e0081799
mohamedmostafa33 <mohamedabohamama15@gmail.com>
2025-04-28 10:33:00 +0200

    Register AnalysisImage model in admin with custom display and image preview

--- backend/diabetescare/admin.py
+++ backend/diabetescare/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from .models import GlucoseTracking
+from .models import GlucoseTracking, AnalysisImage  
 
 @admin.register(GlucoseTracking)
 class GlucoseTrackingAdmin(admin.ModelAdmin):
@@ -48,3 +48,19 @@ class GlucoseTrackingAdmin(admin.ModelAdmin):
             if normal_range and (obj.glucose_value < normal_range[0] or obj.glucose_value > normal_range[1]):
                 extra_context['warning'] = "This reading is abnormal. Please review."
         return super().change_view(request, object_id, form_url, extra_context=extra_context)
+
+@admin.register(AnalysisImage)
+class AnalysisImageAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'description', 'uploaded_at', 'image_preview')
+    list_filter = ('patient', 'uploaded_at')
+    search_fields = ('description', 'patient__first_name', 'patient__last_name')
+    list_display_links = ('patient', 'description')
+    ordering = ('-uploaded_at',)
+    fields = ('patient', 'image', 'description', 'uploaded_at')
+    readonly_fields = ('uploaded_at',)
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 50px;"/>', obj.image.url)
+        return "No Image"
+    image_preview.short_description = 'Image Preview'
